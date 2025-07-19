2f4ded3d24e1db3ec95de4b8c541f4b7cbfcaf13
karthikr78677 <karthikr78677@gmail.com>
2025-07-19 20:22:31 +0530

    Update ProductList.jsx

--- client/frontend/src/ProductList.jsx
+++ client/frontend/src/ProductList.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 // const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3001";
-const API_BASE = "http://localhost:3001";
+const API_BASE = "https://dashboard-admin-backend-tqiy.onrender.com";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
